1e484bfaec1e2daf2eb937a24fbf01b3810329dd
Maarten Verbandt; u0098208 <maarten.verbandt@kuleuven.be>
2016-05-06 13:12:17 +0200

    simplify communicator interface

--- Arduino/libraries/microOS/communicator_interface.cpp
+++
@@ -1,1 +0,0 @@
-#include "communicator_interface.h"
--- Arduino/libraries/microOS/communicator_interface.h
+++ Arduino/libraries/microOS/communicator_interface.h
@@ -4,38 +4,17 @@
 #include <inttypes.h>
 
 class CommunicatorInterface
-{
-protected:
-	uint32_t _async_flag;
-	uint8_t _receive_channel;
-	unsigned long _receive_time;
-	unsigned long _transmission_time;
-	unsigned long _log_time;
-	unsigned long _heartbeat_time;
-	
+{	
 public:
-	CommunicatorInterface():
-		_async_flag(0),
-		_receive_channel(0),
-		_receive_time(0),
-		_transmission_time(0),	
-		_log_time(0),
-		_heartbeat_time(0)
-	{
-		//do nothing;
-	}
-	
-	virtual int start(){ return 0; };
-	virtual int update(){ return 0; };
+	virtual void init();
 
-	uint32_t asyncFlag(){ return _async_flag; };
-	uint32_t checkAsyncFlag(uint32_t async_flag){ return (_async_flag & async_flag); };
-	uint32_t setAsyncFlag(uint32_t async_flag){ return _async_flag = (_async_flag | async_flag); };
-	uint32_t removeAsyncFlag(uint32_t async_flag){ return _async_flag = (_async_flag & (~async_flag)); };
-	
+	virtual void receive();
+
+	virtual void sendHeartbeat();
 	virtual void sendThreadInfo(uint8_t ID, char* name, uint8_t priority,
-							 uint32_t duration, uint32_t latency, 
-							 uint32_t total_duration, uint32_t total_latency, uint32_t number_of_executions){};
+							 	uint32_t duration, uint32_t latency, 
+							 	uint32_t total_duration, uint32_t total_latency, uint32_t number_of_executions);
+	virtual void sendGPIO();
 	//virtual void sendHwinfo()
 };
 
